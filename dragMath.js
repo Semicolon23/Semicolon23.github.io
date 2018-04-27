9faae930ad635c9d1ae3f2c0207fcd3d8197b4d2
unknown <colinmcleodmunreo@mymail.mines.edu>
2018-04-26 20:09:19 -0600

    lskdjf

--- dragMath.js
+++ dragMath.js
@@ -9,7 +9,7 @@ function init() {
   $('#cardSlots').html( '' );
 
   var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-  var terms = ['Why did the scarecrow get a promotion?', 'Why is 6 afraid of 7?', 'What is love?', 'What is the air speed velocity of an unladen swallow?', 'What is my favorite color?', 'Why do they fence in graveyards?', 'How do you get a nun pregnant?', 'Whats the difference between you and a dead prostitute?', 'Guess what my favorite joke is', 'Whats the difference between Michael Jackson and Neil Armstrong?' ];
+  var terms = ['Why did the scarecrow get a promotion?', 'Why is 6 afraid of 7?', 'What is love?', 'What is the air speed velocity of an unladen swallow?', 'What is my favorite color?', 'Why do they fence in graveyards?', 'What is the last letter of the Greek alphabet', 'What US state flag does the Chiliation flag resemble?', 'Guess what my favorite joke is', 'Guess what my second favorite joke is' ];
   <!--numbers.sort( function() { return Math.random() - .5 } );-->
 
   for ( var i=0; i<10; i++ ) {
@@ -25,7 +25,7 @@ function init() {
   shuffle(terms);
 
   // Create the card slots
-  var words = [ 'He was outstanding in his field. HAHAHAHA', '7 was a registered six offender', 'Baby dont hurt meee', 'Is it African or European? HAHAHA', 'Blue', 'People are always dying to get in', 'Dress her as an alter boy. POOR TASTE', 'You still suck. Burnnnnnnnnnn', 'You', 'Armstrong was the first man on the moon. Jackson screwed kids.' ];
+  var words = [ 'He was outstanding in his field. HAHAHAHA', '7 was a registered six offender', 'Baby dont hurt meee', 'Is it African or European? HAHAHA', 'Blue', 'People are always dying to get in', 'Omega', 'The state of Texas', 'You', 'Prefix operators' ];
   for ( var i=1; i<=10; i++ ) {
     $('<div>' + words[i-1] + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
       accept: '#cardPile div',
